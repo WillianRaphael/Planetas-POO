6e0d989bcd1fd6bcc790c83ed14ce4c0a6947ecc
WillianRaphael <willrapha1@gmail.com>
2023-03-16 17:18:12 -0300

    Initial commit

---
+++ CalculoPesoPlaneta.java
@@ -0,0 +1,18 @@
+public class CalculoPesoPlaneta {
+    public static void main(String[] args) {
+        SwitchPlaneta switchPlanet = new SwitchPlaneta();
+
+        InputPeso inputPeso = new InputPeso();
+
+        EscolhaPlaneta escolhaPlaneta = new EscolhaPlaneta();
+
+        double peso = inputPeso.readDouble("Digite seu peso (Kg): ");
+
+        EscolhaPlaneta.ImprimePlaneta();
+
+        int planet = escolhaPlaneta.readInt();
+        double resultado = switchPlanet.Switch(planet, peso);
+
+        System.out.println(resultado);
+    }
+}
